80e277e63c01b00565d0c8cacfbc3b16785616ae
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-04 15:29:01 +0200

    fix: correct comment formatting in last_digit.py

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
-digit = abs(number) % 10
+digit = abs(number) % 10  #abs = valeur absolue
 if digit > 5:
     print(f"Last digit of {number} is {digit} and is greater than 5")
 elif digit == 0:

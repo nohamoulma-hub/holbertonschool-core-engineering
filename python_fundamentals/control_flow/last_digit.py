3c305a89af05e7e85eb8bed126e1c15cdb2c232b
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-04 15:45:32 +0200

    fix: correct condition for negative digit calculation in last_digit.py

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 number = __import__('random').randint(-10000, 10000)
 digit = abs(number) % 10  # abs = valeur absolue
-if digit < 0:
+if number < 0:
     digit = -digit
+
 if digit > 5:
     print(f"Last digit of {number} is {digit} and is greater than 5")
 elif digit == 0:

84d2cf182a962ec5acdf741ccac42a71b29040be
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-04 15:09:49 +0200

    feat: add last_digit.py to demonstrate random number last digit evaluation

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+number = __import__('random').randint(-10000, 10000)
+digit = abs(number) % 10
+if digit > 5:
+    print(f"Last digit of {number} is {digit} and is greater than 5")
+elif digit == 0:
+    print(f"Last digit of {number} is {digit} and is 0")
+else:
+    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")

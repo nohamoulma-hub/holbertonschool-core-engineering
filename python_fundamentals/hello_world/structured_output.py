47935c9ae321c69cb9d7f85b9371122a60db551b
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-04 13:24:49 +0200

    Fix formatting of computation line in structured_output.py

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -2,7 +2,7 @@
 language = "Python"
 version = 3
 pi = 3.14159
-computation = round (pi, 2) == 3.14 #on vérifie que l'arrondi à 2 décimal (3,14) est égal à 3,14
+computation = round(pi, 2) == 3.14  # on vérifie que l'arrondi est égal à 3,14
 print(f"Language: {language}")
 print(f"Version: {version}")
 print(f"Pi approx: {pi:.2f}")

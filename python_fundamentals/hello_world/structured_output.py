a25d834f4502b13802e8164ce684fc2243deaddb
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-04 13:21:48 +0200

    Add structured_output.py for basic Python output demonstration

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+language = "Python"
+version = 3
+pi = 3.14159
+computation = round (pi, 2) == 3.14 #on vérifie que l'arrondi à 2 décimal (3,14) est égal à 3,14
+print(f"Language: {language}")
+print(f"Version: {version}")
+print(f"Pi approx: {pi:.2f}")
+print(f"Computation valid: {computation}")

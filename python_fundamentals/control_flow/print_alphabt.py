9148163d8676e710a2a4bf639e4c6a68e940f3be
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-05 09:59:45 +0200

    fix: optimize variable usage in print_alphabt.py

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 for i in range(97, 123):
-    letter = chr(i)
     if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print(f"{chr(i)}", end="")
+    print("{}".format(chr(i)), end="")

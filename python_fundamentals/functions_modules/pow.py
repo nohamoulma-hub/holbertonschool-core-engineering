f5cb18e7ee8ff7b880a7cc8af534f1f65db7985b
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-06 11:08:23 +0200

    fix: streamline pow function logic for handling zero and negative exponents

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 def pow(a, b):
+
     exposant = b
+
     if b < 0:
         b = -b
-
+    if b == 0:
+        return 1
+    
     result = 1
-
     for i in range(exposant):
-        if b <= 0:
-            return 1
         result = result * a
+        if b < 0:
+            return 1 / result
 
     return result

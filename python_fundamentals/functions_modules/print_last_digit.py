c2a8da1116b622389171f7071cf072a032c51ef5
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-06 10:15:21 +0200

    feat: add print_last_digit function to calculate and print the last digit of a number

---
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    digit = abs(number) % 10  # pour connaitre de le dernier chiffre
+    print(digit)
+    return digit  # return est nécessaire pour que digit soit réutilisable

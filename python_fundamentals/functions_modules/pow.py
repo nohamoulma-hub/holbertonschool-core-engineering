1685c3c68459135d68f608c868a57fd614883d49
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-06 11:41:58 +0200

    fix: update comments in pow function for clarity on exponent handling

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -8,6 +8,13 @@ def pow(a, b):
         exposant = b
     else:
         exposant = -b
+        # sert pour l'utilisation de la boucle
+        # mais au final l'exposant reste positif
+        # car -(-3) = 3
+        # si on utilisait b à la place de exposant
+        #  dans la boucle ci-dessous ça créerait une
+        # erreur car la boucle ne prend pas en charge le négatif
+        # ça voudrait dire parcourt i pendant -3 fois -> Pas possible
 
     result = 1
     for i in range(exposant):
@@ -15,5 +22,6 @@ def pow(a, b):
 
     if b < 0:
         return 1 / result
-
+        # # b et pas exposant car on a besoin de b
+        # pour savoir si b est négatif de base
     return result

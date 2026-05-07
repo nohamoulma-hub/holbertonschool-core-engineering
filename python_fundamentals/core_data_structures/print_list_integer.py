52aaf61a474b9979138539a4b72307e89dc4e2f0
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-07 11:05:19 +0200

    fix: update print_list_integer function to correctly print list elements

--- python_fundamentals/core_data_structures/print_list_integer.py
+++ python_fundamentals/core_data_structures/print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
 if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        print("{:d}".format(my_list))
+    print_list_integer(my_list=[])

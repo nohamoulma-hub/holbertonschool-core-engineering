9996b623342f0f2643ee85c107ce12786f1fe22b
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-07 13:13:41 +0200

    fix: correct return values in replace_in_list function for index validation

--- python_fundamentals/core_data_structures/replace_in_list.py
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     taille = len(my_list)
     if idx < 0 or idx >= taille:
-        return my_list[idx]
+        return my_list
     else:
         my_list[idx] = element
-        return my_list[element]
+        return my_list

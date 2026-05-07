4ead7b86447789075106891253dd4569c20e3831
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-07 14:11:34 +0200

    fix: update print_matrix_integer function to ensure proper line endings

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for i in line:
             if i != line[-1]:
-                print("{:d} ".format(i))
+                print("{:d} ".format(i), end="")
             else:
-                print("{:d}".format(i))
+                print("{:d}".format(i), end="\n")

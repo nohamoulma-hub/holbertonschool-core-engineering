8b3a60b588606c8ac76ec4cfd91bc2508d4b96dc
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-07 14:08:19 +0200

    fix: refactor print_matrix_integer function to improve element printing logic

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
-        print(*line, sep=' ')
+        for i in line:
+            if i != line[-1]:
+                print("{:d} ".format(i))
+            else:
+                print("{:d}".format(i))

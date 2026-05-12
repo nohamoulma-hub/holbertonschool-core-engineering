a26c4e3a3db05e618ad4b8995141a9c2631ac52e
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-12 10:51:05 +0200

    refactor: enhance safe_print_list function for improved element printing

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+"""Module for safe list printing"""
+
+
 def safe_print_list(my_list=[], x=0):
-    try:
-        print(my_list[x])
-    except IndexError:
-        return my_list
+    """Prints x elements of a list and returns the real count printed"""
+    compteur = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            compteur += 1
+        except IndexError:
+            if x > compteur:
+                break
+    print()
+    return compteur

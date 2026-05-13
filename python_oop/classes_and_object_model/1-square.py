90040cc9542e91969528743622e8d4b96fc21a0a
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-13 11:39:17 +0200

    feat: implement Square class with size attribute and constructor

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Define a new class Square with an attribut size """
+
+
+class Square:
+    """ init the size of the square"""
+
+    def __init__(self, size):
+        """ def a private instance"""
+        self.__size = size

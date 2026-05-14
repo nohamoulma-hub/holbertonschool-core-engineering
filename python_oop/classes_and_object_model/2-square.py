2e90664f02ca7258d5a4a098b978d2824b878406
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-14 10:18:19 +0200

    fix: correct Square class constructor to include size validation and default value

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -2,13 +2,15 @@
 """ Define a new class Square with an attribut size and condition"""
 
 
-class Square:
+class Square():
     """ Condition declaration for size"""
 
-    def __init__(self, size):
-        """ def a private instance"""
-        self.__size = size
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        
         if size < 0:
             raise ValueError("size must be >= 0")
-        if isinstance(size, int):
-            raise TypeError("size must be an integer")
+        
+        self.__size = size

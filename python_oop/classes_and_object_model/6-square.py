d5259360f1e2ae39a94f9c8afa35221796c34ed2
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-15 10:39:22 +0200

    fix: correct typo in position validation error message in Square class

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -35,7 +35,7 @@ class Square():
                 not isinstance(value[1], int) or
                 value[0] < 0 or
                 value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integer")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def __build_string(self):

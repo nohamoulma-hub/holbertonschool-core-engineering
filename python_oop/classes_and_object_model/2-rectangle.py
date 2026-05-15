1070455ad7e4f4a0737d5660a8b2dda091e3e8d1
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-15 16:24:26 +0200

    fix: correct area calculation and perimeter logic in Rectangle class

--- python_oop/classes_and_object_model/2-rectangle.py
+++ python_oop/classes_and_object_model/2-rectangle.py
@@ -34,9 +34,9 @@ class Rectangle():
         self.__height = value
 
     def area(self):
-        return self.__width ** 2
+        return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width <= 0 or self.__height <= 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width + self.__height) * 2
+        return (self.__width * 2) + (self.__height * 2)

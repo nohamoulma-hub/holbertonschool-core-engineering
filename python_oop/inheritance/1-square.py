9d918904f7c0d6d89d8cc853bfa97dd00a1f0c72
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-16 11:42:38 +0200

    style: add docstring to Square class for improved documentation

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -6,6 +6,8 @@ Rectangle = __import__('2-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ Square class """
+
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)

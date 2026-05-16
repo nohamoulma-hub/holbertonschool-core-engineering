374f548e8560872d4560e6ef62b7bc626050ce39
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-16 15:40:45 +0200

    feat: add Dragon class with SwimMixin and FlyMixin for swimming and flying capabilities

---
+++ python_oop/abc/dragon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Design a mixin called SwimMixin"""
+
+
+class SwimMixin():
+    def swim(self):
+        print("The creature flies!")
+
+
+class FlyMixin():
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")

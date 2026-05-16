6ba48d75325c04e191295754a147db2626beeaf5
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-16 11:20:59 +0200

    feat: implement Rectangle class with initialization and area method

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Rectangle class"""
+
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

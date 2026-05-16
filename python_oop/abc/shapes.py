1bc0c4c3feb90dad624b41d10f66e9ace35cce71
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-16 14:53:55 +0200

    feat: implement Shape, Circle, and Rectangle classes with area and perimeter methods

---
+++ python_oop/abc/shapes.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" Abstrac method """
+
+
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    """Abstract base method for shape"""
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """This class represent a circle"""
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * pi * self.radius
+
+
+class Rectangle(Shape):
+    """This class represent a Rectangle"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (self.width * 2) + (self.height * 2)
+
+
+def shape_info(shape):
+    print(f"Area:{shape.area()}")
+    print(f"Area:{shape.perimeter()}")

23dc5c348953cc12fa84027a94cb6e57d9221f27
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-16 13:55:18 +0200

    feat: implement Animal class with Dog and Cat subclasses

---
+++ python_oop/abc/animals.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Abstrac class"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"

629841943d8f43a0bb42cc4701d7c2f4b513eb02
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-26 14:30:12 +0200

    feat: add write_file function for writing text to a file

---
+++ python_advanced/file_handling/write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env/python3
+""" Module add a string in a file"""
+
+
+def write_file(filename="", text=""):
+    """ Write text in a exist file or create if not exist"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)

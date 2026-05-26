9ef431729b3fc94fcf973e44174ecb4ebaed43d5
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-26 13:37:40 +0200

    fix: update docstrings for read_file function and module

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-""" """
+""" Module who read a file"""
+
 
 def read_file(filename=""):
-    """ """
+    """ read a file """
     with open(filename, encoding="uft-8") as f:
         print(f.read(), end="")

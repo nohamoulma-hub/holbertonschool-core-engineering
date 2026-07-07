a0eaeb29c16c56839ab30fe55cad672505e63380
nohamoulma-hub <noham.oulma@gmail.com>
2026-07-07 16:08:04 +0200

    feat: implement WebSocket client in ws_client.py

---
+++ python_advanced/websockets/ws_client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Implement a WebSocket client """
+
+
+import asyncio
+from websockets.asyncio.client import connect
+
+
+async def hello():
+    async with connect("ws://localhost:8765") as websocket:
+        await websocket.send("Hello WebSocket\n")
+        message = await websocket.recv()
+        print(message)
+
+
+if __name__ == "__main__":
+    asyncio.run(hello())
